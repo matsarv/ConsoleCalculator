b544ce317162df6361ce475e190e5b363bf6c357
matsarv <mats.arvidsson@outlook.com>
2019-01-18 15:22:10 +0100

    Add loop Clean code Documentation

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -4,19 +4,24 @@ namespace ConsoleCalculator
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
             var keepAlive = true;
             while (keepAlive)
             {
+
                 try
                 {
                     Console.WriteLine(" Welcome to the Calculator \n ");
 
                     Console.WriteLine(" Type of operation ( +  -  /  * ) or x to exit:");
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.Write("> ");
+                    Console.ResetColor();
 
-                    string stringOperation = Console.ReadLine();
+                    string stringOperation = (Console.ReadLine() ?? "");
 
                     switch (stringOperation)
                     {
@@ -47,10 +52,13 @@ namespace ConsoleCalculator
                             break;
                     }
 
-                    Console.WriteLine("\n\n Enter to continue or x to quit!");
+                    Console.WriteLine("\n\n Press Enter to continue or x to quit!");
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.Write("> ");
+                    Console.ResetColor();
 
                     string assigmentKey = Console.ReadLine();
+                    assigmentKey = assigmentKey.ToLower();
 
                     if (assigmentKey == "x")
                     {
@@ -60,7 +68,6 @@ namespace ConsoleCalculator
 
                     Console.Clear();
                 }
-
                 catch
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
@@ -74,28 +81,40 @@ namespace ConsoleCalculator
 
         }
 
+        // <summary>
+        // Will ask for numbers and perform Addition and present result
+        // Will ask to continue add numbers for Addition
         public static double RunAdditionMethod()
         {
             Console.WriteLine(" Type first number:");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
+            Console.ResetColor();
 
             double firstNumber = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine(" Type second number:");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
+            Console.ResetColor();
 
             double secondNumber = Convert.ToDouble(Console.ReadLine());
 
             double result = firstNumber + secondNumber;
 
             Console.WriteLine(" The result is:");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
+            Console.ResetColor();
             Console.WriteLine(firstNumber + " + " + secondNumber + " = " + result);
 
-            Console.WriteLine("\n Continue add number?(y/n)? ");
+            Console.WriteLine("\n Continue add number? (y/n) ");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
+            Console.ResetColor();
 
             string assignmentChoise = (Console.ReadLine() ?? "");
+            assignmentChoise = assignmentChoise.ToLower();
 
             if (assignmentChoise == "y")
             {
@@ -103,31 +122,57 @@ namespace ConsoleCalculator
             }
 
             double RunAdditionMethod = result;
+
             return RunAdditionMethod;
         }
-
+        // <summary>
+        // Will ask for numbers and perform Substraction and present result
+        // Will ask to continue add numbers for Substraction
         public static double RunSubstractionMethod()
         {
             Console.WriteLine(" Type first number:");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
+            Console.ResetColor();
 
             double firstNumber = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine(" Type second number:");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
+            Console.ResetColor();
 
             double secondNumber = Convert.ToDouble(Console.ReadLine());
 
             double result = firstNumber - secondNumber;
 
             Console.WriteLine(" The result is:");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
-            Console.Write(firstNumber + " - " + secondNumber + " = " + result);
+            Console.ResetColor();
+            Console.WriteLine(firstNumber + " - " + secondNumber + " = " + result);
+
+            Console.WriteLine("\n Continue add number? (y/n) ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("> ");
+            Console.ResetColor();
+
+            string assignmentChoise = (Console.ReadLine() ?? "");
+            assignmentChoise = assignmentChoise.ToLower();
+
+            if (assignmentChoise == "y")
+            {
+                RunSubstractionLoopMethod(result);
+            }
 
             double RunSubstractionMethod = result;
+
             return RunSubstractionMethod;
         }
 
+        // <summary>
+        // Will ask for numbers and perform Multiplication and present result
+        // Will ask to continue add numbers for Multiplication
         public static double RunMultiplicationMethod()
         {
             Console.WriteLine(" Type first number:");
@@ -136,20 +181,28 @@ namespace ConsoleCalculator
             double firstNumber = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine(" Type second number:");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
+            Console.ResetColor();
 
             double secondNumber = Convert.ToDouble(Console.ReadLine());
 
             double result = firstNumber * secondNumber;
 
             Console.WriteLine(" The result is:");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
+            Console.ResetColor();
             Console.Write(firstNumber + " * " + secondNumber + " = " + result);
 
             double RunMultiplicationMethod = result;
+
             return RunMultiplicationMethod;
         }
 
+        // <summary>
+        // Will ask for numbers and perform Division and present result
+        // Will ask to continue add numbers for Division
         public static double RunDivisionMethod()
         {
             Console.WriteLine(" Type first number:");
@@ -158,7 +211,9 @@ namespace ConsoleCalculator
             double firstNumber = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine(" Type second number:");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("> ");
+            Console.ResetColor();
 
             double secondNumber = Convert.ToDouble(Console.ReadLine());
 
@@ -175,44 +230,61 @@ namespace ConsoleCalculator
             }
             else
             {
-            Console.WriteLine(" The result is:");
-            Console.Write("> ");
-            Console.Write(firstNumber + " / " + secondNumber + " = " + result);
+                Console.WriteLine(" The result is:");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("> ");
+                Console.ResetColor();
+                Console.Write(firstNumber + " / " + secondNumber + " = " + result);
 
             double RunDivisionMethod = result;
+
             return RunDivisionMethod;
 
             }
         }
 
+        // <summary>
+        // Will ask for numbers and perform Addition Loop and present result
         public static double RunAdditionLoopMethod(double result)
         {
             int i = 0;
             do
             {
-                Console.WriteLine(" Operation:");
-                Console.WriteLine("> +");
-
-                //double firstNumber = result;
+                Console.WriteLine("\n Operation:");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("> ");
+                Console.ResetColor();
+                Console.WriteLine("+");
 
                 Console.WriteLine(" First number:");
-                //Console.Write("> " + firstNumber);
-                Console.Write("> " + result);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("> ");
+                Console.ResetColor();
+                Console.Write(result);
 
                 Console.WriteLine(" \nType second number:");
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("> ");
+                Console.ResetColor();
+                Console.ResetColor();
 
                 double secondNumber = Convert.ToDouble(Console.ReadLine());
 
                 result = result + secondNumber;
 
                 Console.WriteLine(" The result is:");
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("> ");
+                Console.ResetColor();
                 Console.Write(result + " + " + secondNumber + " = " + result);
 
-                Console.WriteLine("\n Continue add number?(y/n)? ");
+                Console.WriteLine("\n\n Continue add number? (y/n) ");
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("> ");
+                Console.ResetColor();
+
                 string assignmentChoise = (Console.ReadLine() ?? "");
+                assignmentChoise = assignmentChoise.ToLower();
 
                 if (assignmentChoise == "n")
                 {
@@ -222,7 +294,62 @@ namespace ConsoleCalculator
             } while (i < 1);
 
             double RunDivisionMethod = result;
+
             return RunDivisionMethod;
+        }
+
+        // <summary>
+        // Will ask for numbers and perform Substraction Loop and present result
+        public static double RunSubstractionLoopMethod(double result)
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine("\n Operation:");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("> ");
+                Console.ResetColor();
+                Console.WriteLine("+");
+
+                Console.WriteLine(" First number:");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("> ");
+                Console.ResetColor();
+                Console.Write(result);
+
+                Console.WriteLine(" \nType second number:");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("> ");
+                Console.ResetColor();
+
+                double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+                result = result - secondNumber;
+
+                Console.WriteLine(" The result is:");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("> ");
+                Console.ResetColor();
+                Console.Write(result + " - " + secondNumber + " = " + result);
+
+                Console.WriteLine("\n\n Continue add number? (y/n) ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("> ");
+                Console.ResetColor();
+
+                string assignmentChoise = (Console.ReadLine() ?? "");
+                assignmentChoise = assignmentChoise.ToLower();
+
+                if (assignmentChoise == "y")
+                {
+                    i = 1;
+                }
+
+            } while (i < 1);
+
+            double RunSubstractionMethod = result;
+
+            return RunSubstractionMethod;
         }
     }
 
